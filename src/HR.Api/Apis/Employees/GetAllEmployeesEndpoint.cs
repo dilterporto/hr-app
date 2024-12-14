2ce8625a785923c298f95b98e50cd94347c354e6
Dilter Porto <contato@dilterporto.com>
2024-12-14 13:36:44 -0300

    add get all

---
+++ src/HR.Api/Apis/Employees/GetAllEmployeesEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using CSharpFunctionalExtensions;
+using FastEndpoints;
+using HR.Application.Contracts;
+using HR.Application.UseCases.GetAllEmployees;
+using MediatR;
+
+namespace HR.Employee.Api.Apis.Employees;
+
+public class GetAllEmployeesEndpoint(IMediator mediator) : EndpointWithoutRequest<IEnumerable<EmployeeResponse>>
+{
+  public override void Configure()
+  {
+    Get("api/employees");
+    AllowAnonymous();  
+  }
+
+  public override async Task HandleAsync(CancellationToken cancellationToken)
+  {
+    var query = new GetAllEmployeesQueryHandler.Query();
+    
+    var result = await mediator.Send(query, cancellationToken);
+
+    await HandleResult(result, cancellationToken);    
+  }
+  
+  private Task HandleResult(Result<IEnumerable<EmployeeResponse>> result, CancellationToken cancellationToken) =>
+    result.IsSuccess ? SendAsync(result.Value, cancellation: cancellationToken) : SendErrorsAsync(cancellation: cancellationToken);
+}
---
+++ src/HR.Application/UseCases/GetAllEmployees/GetAllEmployeesQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CSharpFunctionalExtensions;
+using HR.Abstractions.CQRS;
+using HR.Abstractions.EventSourcing;
+using HR.Application.Contracts;
+using HR.Persistence.Reading.Projections;
+
+namespace HR.Application.UseCases.GetAllEmployees;
+
+public class GetAllEmployeesQueryHandler(IProjectionsReader projectionsReader, IMapper mapper) : IQueryHandler<GetAllEmployeesQueryHandler.Query, Result<IEnumerable<EmployeeResponse>>>
+{
+  public record Query : IQuery<Result<IEnumerable<EmployeeResponse>>>;
+  
+
+  public async Task<Result<IEnumerable<EmployeeResponse>>> Handle(Query request, CancellationToken cancellationToken)
+  {
+    var employeeList = await projectionsReader
+      .GetAllAsync<EmployeeProjection>();
+    
+    return employeeList.AsEnumerable()
+      .Select(mapper.Map<EmployeeResponse>)
+      .ToList();
+  }
+}
