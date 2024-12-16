a237b38dd03c6a0184e5e5779bc868b1fb13018e
Dilter Porto <contato@dilterporto.com>
2024-12-16 09:53:09 -0300

    feat: add get all emplouees use case

--- src/HR.Api/Apis/Employees/GetAllEmployeesEndpoint.cs
+++ src/HR.Api/Apis/Employees/GetAllEmployeesEndpoint.cs
@@ -11,18 +11,17 @@ public class GetAllEmployeesEndpoint(IMediator mediator) : EndpointWithoutReques
   public override void Configure()
   {
     Get("api/employees");
+    Description(x => x.WithTags("Employees"));
     AllowAnonymous();  
   }
 
   public override async Task HandleAsync(CancellationToken cancellationToken)
   {
     var query = new GetAllEmployeesQueryHandler.Query();
-    
     var result = await mediator.Send(query, cancellationToken);
-
-    await HandleResult(result, cancellationToken);    
+    if (result.IsSuccess)
+      await SendAsync(result.Value, cancellation: cancellationToken);
+    else
+      await SendErrorsAsync(cancellation: cancellationToken);
   }
-  
-  private Task HandleResult(Result<IEnumerable<EmployeeResponse>> result, CancellationToken cancellationToken) =>
-    result.IsSuccess ? SendAsync(result.Value, cancellation: cancellationToken) : SendErrorsAsync(cancellation: cancellationToken);
 }
